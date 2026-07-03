3f8f78e7889f8ff195b172f9e5aba2fd751ac81e
josuekengne7 <josue.kengne@facsciences-uy1.cm>
2026-07-03 09:56:02 +0100

    Initial implementation of config seed script

---
+++ 00_config_seed.py
@@ -0,0 +1,29 @@
+import unicodedata
+def normaliser_nom(nom_chef):
+    texte=nom_chef.upper()
+    texte=''.join(
+        caractere for caractere in unicodedata.normalize('NFD', texte)
+        if unicodedata.category(caractere) != 'Mn'
+    )
+    texte=''.join(
+        caractere for caractere in texte
+        if caractere.isalnum() or caractere.isspace()
+    )
+    return texte
+def generer_graine(chaine):
+    seed=0
+    modulo=2147483647
+    for position, caractere in enumerate(chaine, start=1):
+        if 'A' <= caractere <= 'Z':
+            valeur=ord(caractere)-ord('A')+1
+        else:
+            valeur=0
+
+        seed=(seed*37+position*valeur)%modulo
+    return seed
+nom_chef="KENGNE GUEPOUOKSI Pierre Josué"
+chaine_normalisee=normaliser_nom(nom_chef)
+graine=generer_graine(chaine_normalisee)
+
+print("Chaine utilisée :", chaine_normalisee)
+print("Graine du groupe :", graine)
